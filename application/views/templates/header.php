5985fbdffdf1569ad6ab83b25fc2ad89c393e040
Mateusz Kaczmarkiewicz <matt327k@gmail.com>
2020-12-13 16:27:54 +0100

    init

---
+++ application/views/templates/header.php
@@ -0,0 +1,24 @@
+<html>
+	<head>
+		<title>MyBlog</title>
+		<link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css">
+		<link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/style.css">
+	</head>
+
+	<body>
+		<div class="container">
+			<div class="bs-component">
+	          <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+	            <a class="navbar-brand" href="<?php echo base_url(); ?>">MyBlog</a>
+	            <div class="collapse navbar-collapse" id="navbarColor01">
+	              <ul class="navbar-nav mr-auto">
+	                <li><a class="nav-link" href="<?php echo base_url(); ?>">Home</a></li>
+	                <li><a class="nav-link" href="<?php echo base_url(); ?>about">About</a></li>
+	                <li><a class="nav-link" href="<?php echo base_url(); ?>posts">Posts</a></li>                      
+	              </ul>
+	            </div>
+	          </nav>
+	      	</div>
+	    </div>
+
+      	<div class="container">
